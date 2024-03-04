d4e122ad137b2e66be035542d0003aea8c6a8317
vinay vanga <vinayreddyvanga@gmail.com>
2024-03-03 23:01:53 -0500

    removing aws access and secret keys from config

--- src/main/java/com/uploader/s3uploader/profile/UserProfileController.java
+++ src/main/java/com/uploader/s3uploader/profile/UserProfileController.java
@@ -5,10 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -16,6 +13,7 @@ import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/user-profile")
+@CrossOrigin("*")
 public class UserProfileController {
 
     private UserProfileService userProfileService;
