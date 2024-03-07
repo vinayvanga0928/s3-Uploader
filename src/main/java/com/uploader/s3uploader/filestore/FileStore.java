1972b41aa9568775916c8f9528cb4af9530a9f0d
vinay vanga <vinayreddyvanga@gmail.com>
2024-03-06 22:09:38 -0500

    Java backend setup for uploading image file to s3 bucket

--- src/main/java/com/uploader/s3uploader/filestore/FileStore.java
+++ src/main/java/com/uploader/s3uploader/filestore/FileStore.java
@@ -5,6 +5,7 @@ import com.amazonaws.services.rdsdata.model.Value;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Service;
 
 import java.io.InputStream;
--- src/main/java/com/uploader/s3uploader/profile/UserProfileService.java
+++ src/main/java/com/uploader/s3uploader/profile/UserProfileService.java
@@ -1,21 +1,32 @@
 package com.uploader.s3uploader.profile;
 
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.extern.slf4j.Slf4j;
+import com.uploader.s3uploader.bucket.BucketName;
 import com.uploader.s3uploader.datastore.FakeUserProfileDataStore;
+import com.uploader.s3uploader.filestore.FileStore;
+import org.apache.http.entity.ContentType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-import java.util.UUID;
+import javax.swing.text.AbstractDocument;
+import java.io.IOException;
+import java.util.*;
 
+import static org.apache.http.entity.ContentType.*;
 
+@Slf4j
 @Service
 public class UserProfileService {
 
     private final UserProfileDataAccessService userProfileDataAccessService;
+    private final FileStore fileStore;
 
-    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService){
+    @Autowired
+    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService, FileStore fileStore){
         this.userProfileDataAccessService = userProfileDataAccessService;
+        this.fileStore = fileStore;
     }
 
     public List<UserProfile> getUserProfiles(){
@@ -25,21 +36,39 @@ public class UserProfileService {
     public void uploadUserProfileImage(UUID userProfileId, MultipartFile file){
 
         //check if the image is not empty
-        //Check if the file is an image file
-        String fileName = file.getName();
-
-        if (file.isEmpty() && !isImageFile(fileName)){
-            System.out.println("Uploaded file is either empty or not an image file");
+        if (file.isEmpty()){
+            throw new IllegalStateException("The uploaded file is empty [ "+ file.getSize()+" ]");
         }
+
+        //Check if the file is an image file
+        if (!Arrays.asList(IMAGE_JPEG.getMimeType(), IMAGE_PNG.getMimeType(), IMAGE_GIF.getMimeType())
+                .contains(file.getContentType())) {
+            throw new IllegalStateException("File must be an image ["+ file.getContentType()+" ]");
+        }
+
         //Check if the user exists in DB
-        //Check if the link field is empty
+        UserProfile user = userProfileDataAccessService.getUserProfiles()
+                .stream().filter(userProfile -> userProfile.getUserProfileId().equals(userProfileId))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException(String.format("userProfile %s not found", userProfileId)));
+
+//        get metadata from file
+        Map<String, String> metadata = new HashMap<>();
+        metadata.put("Content-Type", file.getContentType());
+        metadata.put("Content-Length", String.valueOf(file.getSize()));
+
+//        ObjectMetadata metadata = new ObjectMetadata();
+//        metadata.setContentLength(file.getSize());
+//        metadata.setContentType(file.getContentType());
 
         //Upload the image file to s3
+        String path = String.format("%s", BucketName.BUCKET_IMAGE.getBucketName());
+        String fileName = String.format("%s-%s", file.getName(), UUID.randomUUID());
+        try {
+            fileStore.save(path, fileName, Optional.of(metadata), file.getInputStream());
+        } catch (IOException e) {
+            throw new IllegalStateException("Error Uploading file to s3: ", e.fillInStackTrace());
+        }
         //Update DB(userProfileImageLink) with the s3 key
-    }
-
-    public boolean isImageFile(String fileName) {
-        String extension = fileName.substring(fileName.lastIndexOf(".") + 1).toLowerCase();
-        return extension.equals("jpg") || extension.equals("jpeg") || extension.equals("png") || extension.equals("gif") || extension.equals("bmp");
     }
 }
